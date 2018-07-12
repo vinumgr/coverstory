d35328f7c173aa4d4e0702f09e2860e41d692191
Vinod Moger <emb-vinomog@embitel.local>
2018-07-12 19:06:40 +0530

    9th commit

---
+++ src/main/java/pagObjects/SearchPage.java
@@ -0,0 +1,27 @@
+package pagObjects;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import globals.Globals;
+
+public class SearchPage extends Globals{
+	public SearchPage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Define the page Objects
+	
+		@FindBy(xpath="//*[contains(@title, 'Search')]")
+		public WebElement searchIcon;
+		
+		@FindBy(xpath="//*[contains(@id, 'searchInput')]")
+		public WebElement searchBox;
+		
+		@FindBy(xpath="//*[contains(@id, 'search_submit_btn')]")
+		public WebElement searchButton;
+		
+		@FindBy(xpath="//*[@id='content']/div[2]/div/div[2]/div[2]/div/div[1]/h1")
+		public WebElement searchResult;
+	}
